ae0adb8a58178cf30ea592c0137b01072692f599
Sergey <sbaidachni@gmail.com>
2017-05-25 12:09:56 -0700

    InputInjection

---
+++ InputInjectionDemo/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Preview.Injection;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InputInjectionDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            base.OnNavigatedTo(e);
+        }
+
+        const int step = 100;
+        int deltax = step;
+        int deltay = step;
+
+        private async void Timer_Tick(object sender, object e)
+        {
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if ((deltax > 0) && (deltay > 0)) { deltay = -step; deltax = -step; }
+                else { deltay = step; deltax = step; }
+
+                var injector = InputInjector.TryCreate();
+
+                var list = new List<InjectedInputMouseInfo>();
+
+                list.Add(new InjectedInputMouseInfo() { DeltaX = deltax, DeltaY = deltay });
+
+                injector.InjectMouseInput(list);
+            });
+        }
+    }
+}
